a6146c724c866626ec7a06facc25e0d3f27bb23e
Mitchel <mitchel_jager@hotmail.com>
2019-02-06 22:38:33 +0100

    Add more weapons, use temporary dark mode

---
+++ test/controllers/categories_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class CategoriesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get categories_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get categories_show_url
+    assert_response :success
+  end
+
+end
