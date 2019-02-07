0ffd8e274fd183b7da9cc8593e3239ec0c3fd439
Mitchel <mitchel_jager@hotmail.com>
2019-02-07 14:18:45 +0100

    Added files for all weapon categories

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -2,4 +2,24 @@ module ContentHelper
   def assault_rifles
     YAML.load(File.read(Rails.root.join("config/content", "assault_rifles.yml")))
   end
+
+  def sub_machine_guns
+    YAML.load(File.read(Rails.root.join("config/content", "sub_machine_guns.yml")))
+  end
+
+  def pistols
+    YAML.load(File.read(Rails.root.join("config/content", "pistols.yml")))
+  end
+
+  def light_machine_guns
+    YAML.load(File.read(Rails.root.join("config/content", "light_machine_guns.yml")))
+  end
+
+  def shotguns
+    YAML.load(File.read(Rails.root.join("config/content", "shotguns.yml")))
+  end
+
+  def sniper_rifles
+    YAML.load(File.read(Rails.root.join("config/content", "sniper_rifles.yml")))
+  end
 end
