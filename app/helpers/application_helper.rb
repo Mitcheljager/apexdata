d4f60a602ec2d5a5c738bbed58680b365bbc3ed9
Mitchel <mitchel_jager@hotmail.com>
2019-02-08 17:50:50 +0100

    Add weapon images for the majority of weapons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def asset_exist?(path)
+    if Rails.configuration.assets.compile
+      Rails.application.precompiled_assets.include? path
+    else
+      Rails.application.assets_manifest.assets[path].present?
+    end
+  end
 end
