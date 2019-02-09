d8b6561164f2c25c07963125e4d5bf091c7ea7f5
Mitchel <mitchel_jager@hotmail.com>
2019-02-09 21:09:15 +0100

    assets initialize_on_precompile to false

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module ApexData
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
