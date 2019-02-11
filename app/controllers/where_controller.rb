ba23d6eb876b673c6193c701ebb872907995d0f6
Mitchel <mitchel_jager@hotmail.com>
2019-02-11 14:32:26 +0100

    remove slug again and use name

--- app/controllers/where_controller.rb
+++ app/controllers/where_controller.rb
@@ -2,6 +2,6 @@ class WhereController < ApplicationController
   include ContentHelper
 
   def index
-    @items = weapons.select { |weapon| weapon[params[:where].underscore.downcase] == params[:value].downcase }
+    @items = weapons.select { |weapon| weapon[params[:where].underscore.downcase].downcase.gsub(" ", "-") == params[:value].downcase }
   end
 end
