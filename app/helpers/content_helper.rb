e3942dc9b04f00f629cd2f97abfbe2de2902140f
Mitchel <mitchel_jager@hotmail.com>
2019-02-11 23:47:10 +0100

    Add consumables, change navigation

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -27,6 +27,10 @@ module ContentHelper
     YAML.load(File.read(Rails.root.join("config/content", "grenades.yml")))
   end
 
+  def consumables
+    YAML.load(File.read(Rails.root.join("config/content", "consumables.yml")))
+  end
+
   def weapons
     assault_rifles.concat(sub_machine_guns).concat(pistols).concat(light_machine_guns).concat(shotguns).concat(sniper_rifles)
   end
