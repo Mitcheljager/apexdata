1e6fecd76018ade8be5f6a99df1e5f70b2f35b37
Mitchel <mitchel_jager@hotmail.com>
2019-02-21 16:51:31 +0100

    Redirect to root if no request referer

--- app/controllers/alldata_controller.rb
+++ app/controllers/alldata_controller.rb
@@ -1,11 +1,11 @@
 class AlldataController < ApplicationController
   def expanded
     cookies.permanent[:alldata] = "expanded"
-    redirect_to request.referer
+    redirect_to request.referer || root_path
   end
 
   def compact
     cookies.permanent[:alldata] = "compact"
-    redirect_to request.referer
+    redirect_to request.referer || root_path
   end
 end
--- app/controllers/viewtype_controller.rb
+++ app/controllers/viewtype_controller.rb
@@ -1,11 +1,11 @@
 class ViewtypeController < ApplicationController
   def expanded
     cookies.permanent[:viewtype] = "expanded"
-    redirect_to request.referer
+    redirect_to request.referer || root_path
   end
 
   def compact
     cookies.permanent[:viewtype] = "compact"
-    redirect_to request.referer
+    redirect_to request.referer || root_path
   end
 end
