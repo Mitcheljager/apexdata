889eb911fc7ba6956c9bb6449f6a1b279c35e88c
Mitchel <mitchel_jager@hotmail.com>
2019-04-17 17:52:17 +0200

    For case sentitive unique usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :username, presence: true, uniqueness: true, format: { with: /\A[a-z\d][a-z\d-]*[a-z\d]\z/i }
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[a-z\d][a-z\d-]*[a-z\d]\z/i }
   validates :password, presence: true, length: { minimum: 8 }
 end
