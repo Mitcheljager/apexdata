ab856da031933ac00d277a2222c1929f47854330
Mitchel <mitchel_jager@hotmail.com>
2019-04-17 17:38:59 +0200

    Allowed dashses in usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :username, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+  validates :username, presence: true, uniqueness: true, format: { with: /\A[a-z\d][a-z\d-]*[a-z\d]\z/i }
   validates :password, presence: true, length: { minimum: 8 }
 end
