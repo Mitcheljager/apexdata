cbdd12ffe892d10d588255ca6af364f229891269
Dennis B <dennis@betman.nl>
2019-04-23 19:20:16 +0200

    new updates tab

--- app/assets/javascripts/profile_popup.js
+++ app/assets/javascripts/profile_popup.js
@@ -1,13 +1,39 @@
 document.addEventListener("DOMContentLoaded", function() {
   const openClaimMobile = document.querySelector(".profile__claim-popup")
-  openClaimMobile.addEventListener('click', function() {
-    claimPopup("open")
-  })
+  if(openClaimMobile !== null) {
+    openClaimMobile.addEventListener('click', function() {
+      claimPopup("open")
+    })
+  }
 
   const closeClaimMobile = document.querySelector(".profile__claim-popup-close")
-  closeClaimMobile.addEventListener('click', function() {
-    claimPopup("close")
-  })
+  if(closeClaimMobile !== null) {
+    closeClaimMobile.addEventListener('click', function() {
+      claimPopup("close")
+    })
+  }
+
+  const openUpdatePopup = document.querySelectorAll(".profile-stats__update")
+  if(openUpdatePopup !== null){
+    openUpdatePopup.forEach(function (update) {
+      update.addEventListener("click", function () {
+        const parent = this.closest(".profile-stats__updates")
+        const updatePopup = parent.querySelector(".profile__popup")
+        updatePopup.classList.add("active")
+      })
+    })
+  }
+
+  const closeUpdatePopup = document.querySelectorAll(".profile-popup__close")
+  if(closeUpdatePopup !== null){
+    closeUpdatePopup.forEach(function (update) {
+      update.addEventListener("click", function () {
+        const parent = this.closest(".profile-stats__updates")
+        const updatePopup = parent.querySelector(".profile__popup")
+        updatePopup.classList.remove("active")
+      })
+    })
+  }
 })
 
 function claimPopup(target) {
