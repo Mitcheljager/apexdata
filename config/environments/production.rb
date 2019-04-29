e82b9c5fcabb26a3640fca5f5df5ac5e19067fda
Mitchel <mitchel_jager@hotmail.com>
2019-04-29 17:45:14 +0200

    Silence assets logging on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.quiet = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
