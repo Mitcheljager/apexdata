6e89c91469bd73d08af04bc27e694a9cb7fc4a32
Mitchel <mitchel_jager@hotmail.com>
2019-05-02 17:11:17 +0200

    Change STDOUT level to error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,7 @@ Rails.application.configure do
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.log_level = :ERROR
   end
 
   config.public_file_server.headers = {
