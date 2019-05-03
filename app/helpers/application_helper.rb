1a67958313e8f8e8adcdd93713a171159e0b0588
Mitchel <mitchel_jager@hotmail.com>
2019-05-03 20:37:16 +0200

    Adjust badges in leaderboards

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,12 @@ module ApplicationHelper
       return profile.username
     end
   end
+
+  def profile_path_by_uid(uid)
+    profile = ClaimedProfile.find_by_profile_uid_and_checks_completed(uid, 1)
+
+    if profile.present?
+      return profile_path(profile.platform, profile.username)
+    end
+  end
 end
