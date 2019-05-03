a154eb00cc7dd393e236f20b4af5ace5051fc9b7
Mitchel <mitchel_jager@hotmail.com>
2019-05-04 01:14:05 +0200

    remove latest migration

--- db/migrate/20190503221845_change_data_values_of_event_to_float.rb
+++
@@ -1,7 +0,0 @@
-class ChangeDataValuesOfEventToFloat < ActiveRecord::Migration[5.2]
-  def change
-    change_column :event_legend_data, :initial_value, :integer
-    change_column :event_legend_data, :current_value, :integer
-    change_column :event_signups, :total_value, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_03_221855) do
+ActiveRecord::Schema.define(version: 2019_04_30_185834) do
 
   create_table "claimed_profiles", force: :cascade do |t|
     t.integer "user_id"
