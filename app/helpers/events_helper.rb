f3c4de101fc0ff637a1c1f038530bd0a2bb4d618
Mitchel <mitchel_jager@hotmail.com>
2019-05-04 18:00:26 +0200

    Use updated_at rather than last_update

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,6 @@
 module EventsHelper
   def get_last_played_event_legend(uid)
-    event_legend_data = EventLegendData.where(profile_uid: uid).order(last_updated: :desc).first
+    event_legend_data = EventLegendData.where(profile_uid: uid).order(updated_at: :desc).first
 
     return event_legend_data.legend
   end
