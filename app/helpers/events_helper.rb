8b57a90c5a453526225c7f68b2e512158b539137
Mitchel <mitchel_jager@hotmail.com>
2019-05-04 17:55:23 +0200

    Show proper data in event flags

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,3 +1,7 @@
 module EventsHelper
-  
+  def get_last_played_event_legend(uid)
+    event_legend_data = EventLegendData.where(profile_uid: uid).order(last_updated: :desc).first
+
+    return event_legend_data.legend
+  end
 end
