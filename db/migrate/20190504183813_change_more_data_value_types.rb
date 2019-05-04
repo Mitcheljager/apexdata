ea74041292374ed5ad6b495b9b985f8d1758c517
Mitchel <mitchel_jager@hotmail.com>
2019-05-04 20:58:05 +0200

    Drop default before migration

--- db/migrate/20190504183813_change_more_data_value_types.rb
+++ db/migrate/20190504183813_change_more_data_value_types.rb
@@ -7,7 +7,9 @@ class ChangeMoreDataValueTypes < ActiveRecord::Migration[5.2]
     else
       change_column :event_legend_data, :initial_value, "bigint USING CAST(initial_value AS bigint)"
       change_column :event_legend_data, :current_value, "bigint USING CAST(current_value AS bigint)"
+      change_column_default :event_signups, :total_value, nil
       change_column :event_signups, :total_value, "bigint USING CAST(total_value AS bigint)"
+      change_column_default :event_signups, :total_value, 0
     end
   end
 end
