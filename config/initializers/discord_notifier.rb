af2b6b509a9e0f06522c6c647eeeb442f6cec7f3
Mitchel <mitchel_jager@hotmail.com>
2019-05-24 20:38:01 +0200

    Change discord webhook avatar

--- config/initializers/discord_notifier.rb
+++ config/initializers/discord_notifier.rb
@@ -2,7 +2,7 @@ if defined?(Discord)
   Discord::Notifier.setup do |config|
     config.url = ENV["DISCORD_NOTIFICATIONS_WEBHOOK_URL"]
     config.username = "ApexData.gg Notifications"
-    config.avatar_url = ""
+    config.avatar_url = "https://i.imgur.com/plQNFx1.jpg"
 
     # Defaults to `false`
     config.wait = true
