dd398a466dc294f609e616e672feac4d141f5c53
Mitchel <mitchel_jager@hotmail.com>
2019-05-24 19:39:19 +0200

    Build users url instead of using routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
           title ":confetti_ball: A new user has been created!"
           description "User ID: #{ user.id }\nUsername: #{ user.username }"
           color "#357b35"
-          url admin_user_url(user.id)
+          url "#{ request.domain }/users/#{ user.id }"
         end
 
         Discord::Notifier.message(embed)
